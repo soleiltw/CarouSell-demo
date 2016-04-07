22eb2c427cf22286de5b7381217575ce06cf819f
Brian Hu <billy170916@gmail.com>
2016-04-07 11:29:49 +0800

    prototype of page collection view cell

---
+++ CarouSell-demo/PageCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  PageCollectionViewCell.swift
+//  CarouSell-demo
+//
+//  Created by Brian Hu on 4/7/16.
+//  Copyright © 2016 Brian Hu. All rights reserved.
+//
+
+import UIKit
+
+class PageCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var pageImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
