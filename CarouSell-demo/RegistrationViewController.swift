3cecd75d96486c49739dc72a3dd95faf73c5ee04
Brian Hu <billy170916@gmail.com>
2016-04-08 11:25:42 +0800

    updated layout

--- CarouSell-demo/RegistrationViewController.swift
+++ CarouSell-demo/RegistrationViewController.swift
@@ -8,14 +8,15 @@
 
 import UIKit
 
-class RegistrationViewController: UIViewController {
+class RegistrationViewController: UIViewController, UIScrollViewDelegate {
     
     private let pageCellReuseIdentifier = "pageCellReuseIdentifier"
     
     @IBOutlet weak var tutorialCollectionView: UICollectionView!
     @IBOutlet weak var layout: UICollectionViewFlowLayout!
+    @IBOutlet weak var pageControl: UIPageControl!
     
-    let pages = [[Constants.image: "coffee", "title": "drink a coffee", "description": "i like coffee"], ["image": "coffee", "title": "drink a coffee", "description": "i like coffee"], ["image": "coffee", "title": "drink a coffee", "description": "i like coffee"], ["image": "coffee", "title": "drink a coffee", "description": "i like coffee"]]
+    let pages = [[Constants.image: "coffee", Constants.title: "drink a coffee", Constants.description: "i like coffee self.tutorialCollectionView.registerNib(UINib(nibName"], [Constants.image: "airplan", Constants.title: "take a flight", Constants.description: "i like to take a flight"], [Constants.image: "dessert", Constants.title: "see sunset in the dessert", Constants.description: "i like coffee"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +24,10 @@ class RegistrationViewController: UIViewController {
         self.tutorialCollectionView.registerNib(UINib(nibName: "PageCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: pageCellReuseIdentifier)
 
         self.layout.itemSize = CGSize(width: UIScreen.mainScreen().bounds.width, height: 382)
+        
+        self.pageControl.numberOfPages = self.pages.count
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -35,6 +38,15 @@ class RegistrationViewController: UIViewController {
         return 1
     }
     
+    @IBAction func pageChanged(sender: AnyObject) {
+        let x = CGFloat(pageControl.currentPage) * self.tutorialCollectionView.frame.size.width
+        self.tutorialCollectionView.setContentOffset(CGPointMake(x, 0), animated: true)
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        let pageNum = round(scrollView.contentOffset.x / scrollView.frame.size.width)
+        self.pageControl.currentPage = Int(pageNum)
+    }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.pages.count
@@ -44,12 +56,13 @@ class RegistrationViewController: UIViewController {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(pageCellReuseIdentifier, forIndexPath: indexPath) as! PageCollectionViewCell
         
         let page = pages[indexPath.item]
-        cell.pageImageView.image = UIImage(named: page["image"]!)
-        cell.titleLabel.text = page["title"]
-        cell.descriptionLabel.text = page["description"]
+        cell.pageImageView.image = UIImage(named: page[Constants.image]!)
+        cell.titleLabel.text = page[Constants.title]
+        cell.descriptionLabel.text = page[Constants.description]
 
         return cell
     }
+
     /*
     // MARK: - Navigation
 
